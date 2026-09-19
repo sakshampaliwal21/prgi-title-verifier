4cb2bc2d0399bf57c1f1e091de8009081f897431
sakshampaliwal21 <paliwalsaksham@gmail.com>
2026-09-19 18:48:59 +0530

    Done

--- src/rules.js
+++ src/rules.js
@@ -7,6 +7,28 @@ const disallowedWords = [
   "army"
 ];
 
+const periodicityWords = [
+  "daily",
+  "weekly",
+  "monthly",
+  "quarterly",
+  "annual"
+];
+
+const commonPrefixes = [
+  "the",
+  "new",
+  "national",
+  "daily"
+];
+
+const commonSuffixes = [
+  "news",
+  "times",
+  "today",
+  "daily"
+];
+
 function checkDisallowedWords(title) {
   const words = title.toLowerCase().split(" ");
   const found = [];
@@ -20,6 +42,39 @@ function checkDisallowedWords(title) {
   return found;
 }
 
+function checkPeriodicity(title) {
+  const words = title.toLowerCase().split(" ");
+  const found = [];
+
+  for (const word of words) {
+    if (periodicityWords.includes(word)) {
+      found.push(word);
+    }
+  }
+
+  return found;
+}
+
+function checkPrefixSuffix(title) {
+  const words = title.toLowerCase().split(" ");
+  const result = {
+    prefix: "",
+    suffix: ""
+  };
+
+  if (commonPrefixes.includes(words[0])) {
+    result.prefix = words[0];
+  }
+
+  if (commonSuffixes.includes(words[words.length - 1])) {
+    result.suffix = words[words.length - 1];
+  }
+
+  return result;
+}
+
 module.exports = {
-  checkDisallowedWords
+  checkDisallowedWords,
+  checkPeriodicity,
+  checkPrefixSuffix
 };
